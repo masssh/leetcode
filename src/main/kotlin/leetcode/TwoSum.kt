5f1c58bc27ac68742ae7eecc230d6efecdd02b19
masssh <utm.ver.2.0@gmail.com>
2019-04-13 17:39:31 +0900

    Add some tests

--- src/main/kotlin/leetcode/TwoSum.kt
+++ src/main/kotlin/leetcode/TwoSum.kt
@@ -1,7 +1,12 @@
 package leetcode
 
-class TwoSum {
-    fun doubleLoop(num: IntArray, target: Int): IntArray {
+open class TwoSum {
+
+    companion object {
+        const val THRESHOLD = 2000
+    }
+
+    open fun doubleLoop(num: IntArray, target: Int): IntArray {
         for (i in 0 until num.size - 1) {
             for (j in 1 until num.size) {
                 if (num[i] + num[j] == target) return intArrayOf(i, j)
@@ -10,7 +15,7 @@ class TwoSum {
         throw IllegalArgumentException("No combination of numbers found")
     }
 
-    fun indexOf(num: IntArray, target: Int): IntArray {
+    open fun indexOf(num: IntArray, target: Int): IntArray {
         for (i in 0 until num.size) {
             val j = num.indexOf(target - num[i])
             if (j >= 0) return intArrayOf(i, j)
@@ -18,7 +23,7 @@ class TwoSum {
         throw IllegalArgumentException("No combination of numbers found")
     }
 
-    fun parallelIndexOf(num: IntArray, target: Int): IntArray {
+    open fun parallelIndexOf(num: IntArray, target: Int): IntArray {
         val pair = (0 until num.size).toList().parallelStream()
             .map { i -> i to num.indexOf(target - num[i]) }
             .filter { o -> o.second >= 0 }
@@ -27,21 +32,10 @@ class TwoSum {
         return intArrayOf(pair.first, pair.second)
     }
 
-    fun hybrid(num: IntArray, target: Int): IntArray {
-        val size = num.size
-        if (size < 2000) {
-            for (i in 0 until size) {
-                val j = num.indexOf(target - num[i])
-                if (j >= 0) return intArrayOf(i, j)
-            }
-            throw IllegalArgumentException("No combination of numbers found")
-        } else {
-            val pair = (0 until size).toList().parallelStream()
-                .map { i -> i to num.indexOf(target - num[i]) }
-                .filter { o -> o.second >= 0 }
-                .findFirst()
-                .orElseThrow { IllegalArgumentException("No combination of numbers found") }
-            return intArrayOf(pair.first, pair.second)
+    open fun hybrid(num: IntArray, target: Int): IntArray {
+        if (num.size < THRESHOLD) {
+            return indexOf(num, target)
         }
+        return parallelIndexOf(num, target)
     }
 }
--- src/test/kotlin/leetcode/TestTwoSum.kt
+++ src/test/kotlin/leetcode/TestTwoSum.kt
@@ -1,6 +1,10 @@
 package leetcode
 
+import com.nhaarman.mockitokotlin2.spy
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
 import org.amshove.kluent.`should equal`
+import org.amshove.kluent.shouldThrow
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -18,6 +22,7 @@ class TestTwoSum : TestBase() {
 
     @Test
     fun doubleLoop() {
+        // success
         listOf(
             TwoSumParam(intArrayOf(1, 2), 3, intArrayOf(0, 1)),
             TwoSumParam(intArrayOf(0, 1, 2), 3, intArrayOf(1, 2)),
@@ -27,10 +32,18 @@ class TestTwoSum : TestBase() {
         ).forEach { (num, target, expected) ->
             twoSum.doubleLoop(num, target) `should equal` expected
         }
+
+        // fail
+        listOf(
+            TwoSumParam(intArrayOf(1, 2), 4, intArrayOf(0, 1))
+        ).forEach { (num, target, expected) ->
+            { twoSum.doubleLoop(num, target) } shouldThrow IllegalArgumentException::class
+        }
     }
 
     @Test
     fun indexOf() {
+        // success
         listOf(
             TwoSumParam(intArrayOf(1, 2), 3, intArrayOf(0, 1)),
             TwoSumParam(intArrayOf(0, 1, 2), 3, intArrayOf(1, 2)),
@@ -40,10 +53,18 @@ class TestTwoSum : TestBase() {
         ).forEach { (num, target, expected) ->
             twoSum.indexOf(num, target) `should equal` expected
         }
+
+        // fail
+        listOf(
+            TwoSumParam(intArrayOf(1, 2), 4, intArrayOf(0, 1))
+        ).forEach { (num, target, expected) ->
+            { twoSum.doubleLoop(num, target) } shouldThrow IllegalArgumentException::class
+        }
     }
 
     @Test
     fun parallelIndexOf() {
+        // success
         listOf(
             TwoSumParam(intArrayOf(1, 2), 3, intArrayOf(0, 1)),
             TwoSumParam(intArrayOf(0, 1, 2), 3, intArrayOf(1, 2)),
@@ -53,18 +74,33 @@ class TestTwoSum : TestBase() {
         ).forEach { (num, target, expected) ->
             twoSum.parallelIndexOf(num, target) `should equal` expected
         }
+
+        // fail
+        listOf(
+            TwoSumParam(intArrayOf(1, 2), 4, intArrayOf(0, 1))
+        ).forEach { (num, target, expected) ->
+            { twoSum.doubleLoop(num, target) } shouldThrow IllegalArgumentException::class
+        }
     }
 
     @Test
     fun hybrid() {
+        // should call TwoSum#indexOf()
+        var spiedWwoSum = spy(twoSum)
         listOf(
-            TwoSumParam(intArrayOf(1, 2), 3, intArrayOf(0, 1)),
-            TwoSumParam(intArrayOf(0, 1, 2), 3, intArrayOf(1, 2)),
-            TwoSumParam(intArrayOf(0, 1, 2), 2, intArrayOf(0, 2)),
-            TwoSumParam(intArrayOf(0, -2, 4), 2, intArrayOf(1, 2)),
-            TwoSumParam(intArrayOf(1, 2, 3, 4, 5), 8, intArrayOf(2, 4))
-        ).forEach { (num, target, expected) ->
-            twoSum.hybrid(num, target) `should equal` expected
+            TwoSumParam(intArrayOf(1, 2), 3, intArrayOf(0, 1))
+        ).forEach { (num, target) ->
+            spiedWwoSum.hybrid(num, target)
+            verify(spiedWwoSum, times(1)).indexOf(num, target)
+        }
+
+        // should call TwoSum#parallelIndexOf()
+        spiedWwoSum = spy(twoSum)
+        listOf(
+            TwoSumParam((0..TwoSum.THRESHOLD).toList().toIntArray(), 3, intArrayOf(0, 1))
+        ).forEach { (num, target) ->
+            spiedWwoSum.hybrid(num, target)
+            verify(spiedWwoSum, times(1)).parallelIndexOf(num, target)
         }
     }
 }
